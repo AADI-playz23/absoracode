d810155f8169b0ab4ab2f74f8663848bc96cc648
Aadarsh Pandey <aadibabaji12345@gmail.com>
2026-06-24 01:20:59 +0530

    feat: add db/seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -4,6 +4,8 @@ PRAGMA foreign_keys = ON;
 -- Built-in Languages Seeding
 -- ============================================================
 INSERT OR IGNORE INTO languages (id, name, is_builtin) VALUES
+  ('lang-html-css',   'HTML & CSS',  1),
+  ('lang-javascript', 'JavaScript',  1),
   ('lang-python',     'Python',      1),
   ('lang-cpp',        'C++',         1),
   ('lang-java',       'Java',        1),
